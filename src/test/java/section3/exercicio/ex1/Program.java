c95ea9fb1ab630d7cbd28f4ea90fb6c57435c595
s2it_mnascimento <s2it_mnascimento@uolinc.com>
2019-06-05 15:46:17 -0300

    Implement rectangle class

--- src/test/java/section3/exercicio/ex1/Program.java
+++ src/test/java/section3/exercicio/ex1/Program.java
@@ -8,11 +8,16 @@ public class Program {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner();
+        Scanner sc = new Scanner(System.in);
 
-        System.out.printf("Enter width and height: ");
+        Rectangle rectangle = new Rectangle();
+
+        System.out.print("Enter width and height: ");
+        rectangle.width = sc.nextDouble();
+        rectangle.height = sc.nextDouble();
+
+        System.out.println(rectangle.toString());
 
         sc.close();
-
     }
 }
--- src/test/java/section3/exercicio/ex1/Rectangle.java
+++ src/test/java/section3/exercicio/ex1/Rectangle.java
@@ -1,4 +1,25 @@
 package section3.exercicio.ex1;
 
 public class Rectangle {
+
+    public double width;
+    public double height;
+
+    public double area(){
+        return width * height;
+    }
+
+    public double perimeter(){
+        return 2 * (width + height);
+    }
+
+    public double diagonal(){
+        return Math.sqrt(width * width + height * height);
+    }
+
+    public String toString(){
+        return "AREA = " + area()
+                + "\nPERIMETER = " + perimeter()
+                + "\nDIAGONAL = " + diagonal();
+    }
 }
