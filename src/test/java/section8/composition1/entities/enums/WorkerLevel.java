a87692f1253f652bc629b0d4ce4f740a2b692f5d
MichaelBrendo <michael.taqua@hotmail.com>
2019-08-13 00:36:17 -0300

    Enum and composition

---
+++ src/test/java/section8/composition1/entities/enums/WorkerLevel.java
@@ -0,0 +1,8 @@
+package section8.composition1.entities.enums;
+
+public enum WorkerLevel {
+
+    JUNIOR,
+    MID_LEVEL,
+    SENIOR;
+}
---
+++ src/test/java/section8/enum1/entities/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package section8.enum1.entities.enums;
+
+public enum OrderStatus {
+
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+}
